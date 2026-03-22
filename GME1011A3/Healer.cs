b45694885611f54c9adcc33991cd768b498261f1
Priyanshi Patel <A00273233@mycambrian.ca>
2026-03-22 19:42:26 -0400

    Added Healer subclass extending Hero

--- GME1011A3/Healer.cs
+++ GME1011A3/Healer.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 
 namespace GME1011A3
 {
@@ -6,36 +6,55 @@ namespace GME1011A3
     {
         private int dexterity;
 
-        public Healer() : base() => dexterity = 5;
-
-        public Healer(int health, string name, int dexterity) : base(health, name ?? "Healer")
+        public Healer() : base()
         {
-            if (this.health > 80) this.health = 80;
-            this.dexterity = (dexterity < 0 || dexterity > 10) ? 5 : dexterity;
+            this.dexterity = 5;
+        }
+
+        public Healer(int health, string name, int dexterity) : base(health, name)
+        {
+            if (this.health > 80)
+                this.health = 80;
+
+            if (dexterity < 0 || dexterity > 10)
+                dexterity = 5;
+
+            this.dexterity = dexterity;
         }
 
         public override int DealDamage()
         {
-            return new Random().Next(3, 9);
+            Random rng = new Random();
+            return rng.Next(3, 9);
         }
 
-        public override void Heal(int amount)
+        public override void Heal(int health)
         {
-            this.health += amount;
+            this.health += health;
             if (this.health > 80) this.health = 80;
         }
 
+        public int GetDexterity() { return this.dexterity; }
+
+        public void AddDexterity() { if (this.dexterity <= 9) dexterity++; }
+
         public int HealPartyMember()
         {
-            if (dexterity <= 0) return 0;
-            dexterity--;
-            Console.WriteLine("*Party Heal!*");
-            return new Random().Next(20, 35);
+            if (dexterity > 0)
+            {
+                dexterity--;
+                Random rng = new Random();
+                return rng.Next(20, 35);
+            }
+            else
+            {
+                return 0;
+            }
         }
 
         public override string ToString()
         {
-            return $"Healer[{health}, {GetName()}, dex: {dexterity}]";
+            return "Healer[" + base.ToString() + ", dexterity: " + this.dexterity + "]";
         }
     }
 }
