ebf642791d19cae1c114877807e80637d02f08fe
Priyanshi Patel <A00273233@mycambrian.ca>
2026-03-22 18:49:54 -0400

    Added Minion base class

--- GME1011A3/Minion.cs
+++ GME1011A3/Minion.cs
@@ -1,35 +1,35 @@
-﻿using System;
+using System;
 
 namespace GME1011A3
 {
-    internal abstract class Minion
+    internal class Minion
     {
-        protected int health;
-        protected int armour;
+        protected int _health;
+        protected int _armour;
 
         public Minion(int health, int armour)
         {
-            if (health <= 0 || health > 35) health = 35;
-            this.health = health;
-            if (armour < 0 || armour > 5) armour = 3;
-            this.armour = armour;
+            if (health <= 0 || health > 35)
+                health = 35;
+            _health = health;
+
+            if (armour < 0 || armour > 5)
+                armour = 3;
+            _armour = armour;
         }
 
-        public int GetHealth() => health;
-        public int GetArmour() => armour;
+        public int GetHealth() { return _health; }
+        public int GetArmour() { return _armour; }
 
-        public virtual void TakeDamage(int damage)
+        public virtual void TakeDamage(int damage) { _health -= (damage - _armour); }
+
+        public virtual int DealDamage() { return 5; }
+
+        public bool isDead() { return _health <= 0; }
+
+        public override string ToString()
         {
-            int actual = damage - armour;
-            if (actual < 0) actual = 0;
-            health -= actual;
+            return "Minion[" + _health + "," + _armour + "]";
         }
-
-        public abstract int DealDamage();
-
-        public bool isDead() => health <= 0;
-
-        public override string ToString() =>
-            $"Minion[{health}, {armour}]";
     }
 }
