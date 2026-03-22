6fbefc74616d525217cd27879eb241c5db0798f6
Priyanshi Patel <A00273233@mycambrian.ca>
2026-03-22 01:31:15 -0400

    Added Hero base class

--- GME1011A3/Hero.cs
+++ GME1011A3/Hero.cs
@@ -1,25 +1,18 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System;
 
 namespace GME1011A3
 {
     internal class Hero
     {
-        //superclass (or base class) attributes
         protected int health;
         private string name;
 
-        //zero-argument constructor
         public Hero()
         {
             health = 100;
             name = "unnamed hero";
         }
 
-        //argumented constructor
         public Hero(int health, string name)
         {
             if (health < 0 || health > 100) { health = 100; }
@@ -28,14 +21,11 @@ namespace GME1011A3
             this.name = name;
         }
 
-        //simple accessors
         public int GetHealth() { return health; }
         public string GetName() { return name; }
 
-        //helper method (doesn't interact with attributes)
         public virtual int DealDamage() { return 10; }
 
-        //mutators (Heal and TakeDamage affect health)
         public virtual void Heal(int health)
         {
             this.health += health;
@@ -48,14 +38,11 @@ namespace GME1011A3
             if (this.health < 0) { this.health = 0; }
         }
 
-        //Is our hero dead? Check to see if health < 0 - this will be a true/false statement
         public bool isDead() { return health <= 0; }
 
-        //Handy-dandy ToString for debugging
-        public override String ToString()
+        public override string ToString()
         {
             return "Hero[" + health + ", " + name + "]";
         }
-
     }
 }
