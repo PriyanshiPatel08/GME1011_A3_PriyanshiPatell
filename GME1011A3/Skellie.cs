2d907125749c1f8e8b4c4653f5272a023e765679
Priyanshi Patel <A00273233@mycambrian.ca>
2026-03-22 21:06:51 -0400

    Added Skellie subclass extending Minion

--- GME1011A3/Skellie.cs
+++ GME1011A3/Skellie.cs
@@ -1,32 +1,35 @@
-﻿using System;
+using System;
 
 namespace GME1011A3
 {
     internal class Skellie : Minion
     {
-        public Skellie(int health) : base(health, 0) { }
+        public Skellie(int health, int armour) : base(health, armour)
+        {
+            _armour = 0;
+        }
 
         public override void TakeDamage(int damage)
         {
-            int half = damage / 2;
-            if (half < 0) half = 0;
-            health -= half;
+            _health -= damage / 2;
         }
 
         public override int DealDamage()
         {
-            return new Random().Next(2, 9);
+            Random rng = new Random();
+            return rng.Next(2, 8);
         }
 
         public int SkellieRattle()
         {
-            Console.WriteLine("*Spooky rattling...*");
-            return new Random().Next(7, 15);
+            Console.WriteLine("  *** Spooky rattling!! The skellie shakes its bones! ***");
+            Random rng = new Random();
+            return rng.Next(7, 15);
         }
 
         public override string ToString()
         {
-            return $"Skellie[{health}, {armour}]";
+            return "Skellie[" + base.ToString() + "]";
         }
     }
 }
